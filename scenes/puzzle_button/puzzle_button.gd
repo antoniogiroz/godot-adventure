394ceede6ec279a427e9c34bc7293e5543395587
Antonio Giroz <antonioluisgil@gmail.com>
2025-04-18 11:48:36 +0200

    refactor: connect signals from code instead usgin the editor

--- scenes/puzzle_button/puzzle_button.gd
+++ scenes/puzzle_button/puzzle_button.gd
@@ -6,11 +6,15 @@ class_name PuzzleButton extends Area2D
 var _bodies_on_top := 0
 
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("pushable") or body is Player:
 		_bodies_on_top += 1
 		animated_sprite.play("pressed")
-
 
 
 func _on_body_exited(body: Node2D) -> void:
--- scenes/scene_entrance/scene_entrance.gd
+++ scenes/scene_entrance/scene_entrance.gd
@@ -7,6 +7,11 @@ class_name SceneEntrance extends Area2D
 @export var player_spawn_position: Vector2
 
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if not body is Player:
 		return
